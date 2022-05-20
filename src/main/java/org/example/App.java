d0b9419ff9cd05558f6bd14ffece172cefde73e7
Germán <barreragerman27@gmail.com>
2022-05-19 23:58:02 -0300

    Se realizan las pruebas del CRUD de student en el main.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,29 +2,36 @@ package org.example;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.example.entity.Student;
+import org.example.service.serviceImpl.StudentServiceImpl;
+
+import java.util.List;
 
 
 /**
  * Application for Practice Java CRUD
  */
 public class App {
+
     private static final Logger LOGGER = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
 
+        StudentServiceImpl service = new StudentServiceImpl();
+        List<Student> studentList1 = service.listStudentSDAO();
+        for (Student student : studentList1) {
+            LOGGER.info(student.toString() + student.getName());
+        }
 
         /**
          * Here I INSERT a new student to the DB
          */
-//        Student student1 = new Student("Jhon", "Barboza", "456789");
-//        studentDAO.insertEntity(student1);
-//        Student student2 = new Student("Patrick", "Bertola", "78946513");
-//        studentDAO.insertEntity(student2);
-
+//        Student student1 = new Student("Raul","Ibraimovich","789465");
+//        service.insertStudentDAO(student1);
         /**
          * Here I UPDATE an existen element into the DB
          */
-       // IStudentDAO studentDAO = new IStudentDAO();
+        // IStudentDAO studentDAO = new IStudentDAO();
 //        Student modifyingStudent = new Student(7, "Patrick", "Schutts", "78946513");
 //        IStudentDAO updateDAO1 = new IStudentDAO();
 //        updateDAO1.updateEntity(modifyingStudent);
@@ -32,23 +39,23 @@ public class App {
         /**
          * Here I DELETE an element
          */
-//        Student deleteStudent = new Student(1);
-//        studentDAO.deleteEntity(deleteStudent);
+//        Student student3 = new Student(21);
+//        service.deleteStudentDAO(student3);
 
         /**
          * Here I find an element by ID
          */
-//        Student student3 = new Student(5,"Jhon","Barboza","456789");
-//        studentDAO.getEntityBYId(student3);
-//          LOGGER.info(student3);
-        }
+//        Student student2 = new Student(7);
+//        service.getStudentDAOById(7);
+//        LOGGER.info(student2.toString() + student2.getName());
 
         /**
          * Here we create a list of student from the DB
          */
-//        List<Student> students = studentDAO.listEntity();
-//        for (Student student : students) {
-//            LOGGER.info("Student = " + student);
+//        List<Student> studentList = service.listStudentSDAO();
+//        for (Student student : studentList) {
+//            LOGGER.info(student.toString() + student.getName());
 //        }
     }
+}
 
