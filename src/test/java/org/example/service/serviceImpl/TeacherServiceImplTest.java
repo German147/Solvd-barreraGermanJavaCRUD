539c48e50cea703431dd80f95b88c6d2ac2c3695
Germán <barreragerman27@gmail.com>
2022-06-07 23:02:14 -0300

    primer commit after reset

--- src/test/java/org/example/service/serviceImpl/TeacherServiceImplTest.java
+++ src/test/java/org/example/service/serviceImpl/TeacherServiceImplTest.java
@@ -1,37 +1,107 @@
 package org.example.service.serviceImpl;
 
-import junit.framework.TestCase;
 import org.example.entity.Teacher;
+import org.example.exceptions.DAO_exception;
+import org.example.repositoryDAO.DAOImpl.TeacherDAOImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TeacherServiceImplTest extends TestCase {
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class TeacherServiceImplTest {
+
+    TeacherDAOImpl teacherUnderTest = new TeacherDAOImpl();
+
+    @Test
     public void testFindAllTeachers() {
-//        given
+        //given
         List<Teacher> teacherList = new ArrayList<>();
-        Teacher jose = new Teacher(1, "Jose", "Martinez", "123465", 4);
-        Teacher martin = new Teacher(2, "Martin", "Rodriguez", "457891", 4);
-        teacherList.add(jose);
-        teacherList.add(martin);
-//        when
-        TeacherServiceImpl service = new TeacherServiceImpl();
-        teacherList = service.findAllTeachers();
 
-//        after
+        //when
+        teacherList = teacherUnderTest.list();
+        boolean expected = false;
+        if (!teacherList.isEmpty()) {
+            expected = true;
+        }
 
+        //then
+        assertTrue(expected);
     }
 
+    @Test
     public void testGetTeacherById() {
+        try {
+            //given
+            Teacher german = new Teacher(3, "German", "Barrera", "32165487");
+
+            //when
+            Teacher germanUnderTest = new Teacher();
+            germanUnderTest = teacherUnderTest.getById(3);
+
+            //then
+            assertEquals(german, germanUnderTest);
+        } catch (DAO_exception e) {
+            e.printStackTrace();
+        }
     }
 
+    @Test
     public void testUpdateTeacher() {
+        try {
+            //given
+            Teacher tomasToUpdate = new Teacher(1, "Tomas", "Frias", "456789");
+
+            //when
+            teacherUnderTest.update(tomasToUpdate);
+            Teacher afterUpdated = new Teacher();
+            afterUpdated = teacherUnderTest.getById(1);
+
+            //then
+            assertEquals(tomasToUpdate, afterUpdated);
+        } catch (DAO_exception e) {
+            e.printStackTrace();
+        }
     }
 
+    @Test
     public void testInsertTeacher() {
+        try {
+            //given
+            Teacher jose = new Teacher(9, "Jose", "Martinez", "123465");
+            teacherUnderTest.insert(jose);
+
+            //when
+            Teacher joseUnderTest = new Teacher();
+            joseUnderTest = teacherUnderTest.getById(9);
+
+            //then
+            assertEquals(jose, joseUnderTest);
+        } catch (DAO_exception | SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Test
     public void testDeleteTeacherById() {
+        try {
+            //given
+            Teacher teacherToDelete = new Teacher(5);
+
+            //when
+            teacherUnderTest.delete(teacherToDelete);
+            Teacher teacherDoesntExists = new Teacher();
+            teacherDoesntExists = teacherUnderTest.getById(5);
+
+            //then
+            Assertions.assertNull(teacherDoesntExists);
+        } catch (DAO_exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
